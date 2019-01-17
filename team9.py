ffbfaf1e5595f7bb847d9eb2ed89ae63cc99dfcb
Bjorlulu <46571970+Bjorlulu@users.noreply.github.com>
2019-01-17 12:16:19 -0600

    FinishedStrat

--- team9.py
+++ team9.py
@@ -26,7 +26,19 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+def Gamewinner(history):
+    if len(my_history) == 0:        
+          return "c"    
+    if thier_score > 0:        
+           
+        import random
+        number = random.randint(1,10)
+        if number == 10:
+            return "b"
+        else:
+            return "c"
+    else:
+        return "c"
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
